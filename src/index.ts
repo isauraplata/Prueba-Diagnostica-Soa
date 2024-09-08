babffaa8a610d6eee56f5e9fc4138b673c33c0dd
valie <isauravpr@gmail.com>
2024-09-08 14:28:28 -0600

    add rateLimit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv"
 import productRouter from "./products/infrastructure/productRouter";
 import orderRouter from "./orders/infrastructure/orderRouter";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app =express();
 
@@ -13,6 +14,15 @@ app.use(cors());
 const port=process.env.PORT_SERVER;
 const now = new Date();
 
+// Middleware de límite de tasa global
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minuto
+    max: 100, // Máximo de solicitudes por minuto
+    message: 'Demasiadas solicitudes desde esta dirección IP, por favor inténtalo de nuevo más tarde.'
+});
+
+
+app.use(limiter);
 
 app.listen(port,()=>{
     console.log("listening on port: "+port)
